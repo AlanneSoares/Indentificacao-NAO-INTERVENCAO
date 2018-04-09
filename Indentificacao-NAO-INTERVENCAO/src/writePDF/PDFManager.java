0d9dfe3a9544a4a141433dd9549a14319ba088cb
alanne.soares <alannesilva@yahoo.com.br>
2018-04-09 14:51:15 -0300

    .

--- Indentificacao-NAO-INTERVENCAO/src/writePDF/PDFManager.java
+++ Indentificacao-NAO-INTERVENCAO/src/writePDF/PDFManager.java
@@ -2,14 +2,14 @@ package writePDF;
 
 public class PDFManager {
 
-    private PDFParser parser;
-    private PDFTextStripper pdfStripper;
-    private PDDocument pdDoc;
-    private COSDocument cosDoc;
+    //private PDFParser parser;
+    //private PDFTextStripper pdfStripper;
+    //private PDDocument pdDoc;
+    //private COSDocument cosDoc;
 
     private String Text ;
     private String filePath;
-    private File file;
+    //private File file;
 
     public PDFManager() {
 
