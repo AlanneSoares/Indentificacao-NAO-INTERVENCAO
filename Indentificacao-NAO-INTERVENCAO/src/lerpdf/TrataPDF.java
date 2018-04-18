3635e67fc36a9f3489a5443a68a0b2a860284643
alanne.soares <alanne.soares@mprj.mp.br>
2018-04-18 12:47:38 -0300

    Ok

--- Indentificacao-NAO-INTERVENCAO/src/lerpdf/TrataPDF.java
+++ Indentificacao-NAO-INTERVENCAO/src/lerpdf/TrataPDF.java
@@ -1,62 +1,66 @@
-/*
 package lerpdf;
 
-//import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-//import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.SQLException;
-//import java.text.Normalizer;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
+import java.io.*;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
 
-//import org.apache.pdfbox.cos.COSDocument;
-//import org.apache.pdfbox.pdfparser.PDFParser;
-//import org.apache.pdfbox.pdmodel.PDDocument;
-//import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class TrataPDF {
+    public static void main(String args[]) {
 
-    String[] linhasPDF;
-    int ultimaLinhaPDF = 0;
-    int qtde_erros = 0;
+        System.out.println(pdf());
+    }
 
-    PDFTextStripper pdfStripper = null;
-    PDDocument pdDoc = null;
-    COSDocument cosDoc = null;
-    String parsedText= "";
+    public static String pdf(){
 
+        PDFParser parser = null;
+        PDDocument pdDoc = null;
+        COSDocument cosDoc = null;
+        PDFTextStripper pdfStripper;
 
-    public void linhasPDF ( String nomePDF, String nomePDFsemDiretorio) throws SQLException, FileNotFoundException {
-
-        InputStream bin = new FileInputStream(new File(nomePDF));
+        String interiorPdf;
+        String fileName = "C:\\Users\\alanne.soares\\Documents\\teste.pdf";
+        File file = new File(fileName);
         try {
-            PDFParser parser = new PDFParser(bin);
+            parser = new PDFParser(new FileInputStream(file));
             parser.parse();
             cosDoc = parser.getDocument();
             pdfStripper = new PDFTextStripper();
             pdDoc = new PDDocument(cosDoc);
-            pdfStripper.setEndPage(1);
-            parsedText = pdfStripper.getText(pdDoc);
-            //System.out.println(parsedText);
-            bin.close();
-            bin.close();
-        } catch (IOException e) {
-            qtde_erros=1;
-            try {
-                if (cosDoc != null) bin.close();
-                if (pdDoc  != null) bin.close();
-            } catch (Exception e1) {
-                e.printStackTrace();
+            interiorPdf = pdfStripper.getText(pdDoc);
+
+            String conteudoPDFSemAcentuacao = Normalizer.normalize(interiorPdf, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+
+            String conteudoSemEspaco = conteudoPDFSemAcentuacao.replaceAll("\\s+", " ");
+
+            String[] palavras = conteudoSemEspaco.split(" ");
+
+            return conteudoPDFSemAcentuacao.toUpperCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
             }
         }
-        //System.out.println(parsedText);
-        linhasPDF = parsedText.split("\\r?\\n");
-        ultimaLinhaPDF = linhasPDF.length;
-        // System.out.println(" numero de linhas do PDF "+ ultimaLinhaPDF);
+
+
+        public String[] removePreposicao() {
+            List lista = new ArrayList();
+            String[] preposicao;
+
+            String[] preposicao = new String () ["A", "ANTE", "APOS", "ATE", "COM", "CONTRA", "DE", "DESDE", "EM", "ENTRE", "PARA", "PER", "PERANTE", "POR", "SEM", "SOB", "SOBRE", "TRAS"))];
+
+        }
     }
-}
-*/
+
+    //String[] strings = (String[]) lista.toArray (new String[lista.size()]);
